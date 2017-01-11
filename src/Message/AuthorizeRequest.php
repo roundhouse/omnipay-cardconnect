f431e6eedf952e38bd50b34233967af659b38974
Jeremy Bueler <jbueler@gmail.com>
2017-01-11 15:31:14 -0800

    Adding Capture and Purchase to Gateway

--- src/Message/AuthorizeRequest.php
+++ src/Message/AuthorizeRequest.php
@@ -6,39 +6,9 @@ class AuthorizeRequest extends AbstractRequest
 {
     public function getData()
     {
-        //Process and validate data for the request
         $this->validate('amount', 'card');
-        // if ($this->getCard()->validate()) {
-        // }
+        $this->getCard()->validate();
         $card = $this->getCard();
-        // (
-        //     [billingFirstName] => Jeremy
-        //     [shippingFirstName] => Jeremy
-        //     [billingLastName] => Bueler
-        //     [shippingLastName] => Bueler
-        //     [number] => 4242424242424242
-        //     [expiryMonth] => 12
-        //     [expiryYear] => 2018
-        //     [startMonth] => 12
-        //     [startYear] => 2012
-        //     [cvv] => 023
-        //     [issueNumber] =>
-        //     [billingAddress1] => 537 SE Ash St Suite #401
-        //     [shippingAddress1] => 537 SE Ash St Suite #401
-        //     [billingAddress2] =>
-        //     [shippingAddress2] =>
-        //     [billingCity] => Portland
-        //     [shippingCity] => Portland
-        //     [billingPostcode] => 97214
-        //     [shippingPostcode] => 97214
-        //     [billingState] => OR
-        //     [shippingState] => OR
-        //     [billingCountry] =>
-        //     [shippingCountry] =>
-        //     [billingPhone] =>
-        //     [shippingPhone] =>
-        //     [email] => jeremy@roundhouseagency.com
-        // )
     	$data = array(
     		'merchid'   => $this->getMerchantId(),
     		'account'   => $card->getNumber(),
@@ -55,8 +25,6 @@ class AuthorizeRequest extends AbstractRequest
     		'postal'    => $card->getBillingPostcode(),
     		'tokenize'  => "Y",
     	);
-        //
-        // echo "<pre>";print_r($this->getCard()); echo "</pre><br /><br /><br /><br />";
         return $data;
     }
 
